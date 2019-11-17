be3709bf96ff0f0081ba6c1aa003b82a7e382c82
MarcosJr1 <marcosjr.vcs@gmail.com>
2019-11-16 23:04:43 -0300

    changed unitControl to accept and procces funct from instruction

--- src/components/control/ControlUnit/ControlUnit.vhd
+++ src/components/control/ControlUnit/ControlUnit.vhd
@@ -6,85 +6,86 @@ USE ieee.numeric_std.ALL;
 ENTITY ControlUnit IS
     PORT (
         -- POSITION: (0: RegWrite, 1: MEMtoREG)
-        WB : OUT STD_LOGIC_VECTOR(0 TO 1) := "00"; -- Write back
+        WB : OUT STD_LOGIC_VECTOR(0 TO 1) := "00";
         -- POSITION: (0: BRANCH, 1: MEMRead, 2: MEMWrite)
-        MEM : OUT STD_LOGIC_VECTOR(0 TO 2) := "000"; -- Memory
-        -- POSITION: (REGDst, ALUOp(3bits), ALUSrc)
-        EX : OUT STD_LOGIC_VECTOR(0 TO 4) := "00000"; -- Exec
-        -- SIGNAL FOR COOSE PC INPUT
+        MEM : OUT STD_LOGIC_VECTOR(0 TO 2) := "000";
+        -- POSITION: (0: REGDst, 1,2,3: ALUop, 4: ALUsrc)
+        EX : OUT STD_LOGIC_VECTOR(0 TO 4) := "00000";
+        -- SIGNAL FOR ChOOSE PC INPUT
         SIGNAL_JUMP : OUT STD_LOGIC_VECTOR(0 TO 1) := "00";
 
-        OP_CODE : IN STD_LOGIC_VECTOR(0 TO 5) -- Instruction OP Code
+        INSTRUCTION : IN STD_LOGIC_VECTOR(0 TO 31)
     );
 END ControlUnit;
 
 ARCHITECTURE UC OF ControlUnit IS
 BEGIN
-    PROCESS (OP_CODE)
+    PROCESS (INSTRUCTION)
     BEGIN
-        CASE OP_CODE IS
-            WHEN "000001" => --add
-                EX <= "10000";
-                MEM <= "0X0";
+        CASE INSTRUCTION(0 TO 5) IS
+
+            WHEN "000001" => --TYPE R
                 WB <= "10";
-            WHEN "000010" => --sub
-                EX <= "10010";
                 MEM <= "0X0";
-                WB <= "10";
-            WHEN "000011" => --add imed
-                EX <= "10001";
-                MEM <= "0X0";
-                WB <= "10";
-            WHEN "000100" => --sub imed
-                EX <= "10011";
-                MEM <= "0X0";
-                WB <= "10";
-            WHEN "000111" => --And
                 EX <= "10100";
-                MEM <= "0X0";
+
+            WHEN "000010" => --TYPE I ADD
                 WB <= "10";
-            WHEN "001000" => --And imed
-                EX <= "10101";
                 MEM <= "0X0";
-                WB <= "10";
-            WHEN "001001" => --Or
                 EX <= "10110";
-                MEM <= "0X0";
+
+            WHEN "000010" => --TYPE I SUB
                 WB <= "10";
-            WHEN "001010" => --Or imed
-                EX <= "10111";
                 MEM <= "0X0";
+                EX <= "11000";
+
+            WHEN "000010" => --TYPE I AND
                 WB <= "10";
+                MEM <= "0X0";
+                EX <= "11010";
+
+            WHEN "000010" => --TYPE I OR
+                WB <= "10";
+                MEM <= "0X0";
+                EX <= "11110";
+
             WHEN "000101" => --LW
-                EX <= "00001";
-                MEM <= "010";
                 WB <= "11";
-            WHEN "000110" => --SW
+                MEM <= "010";
                 EX <= "00001";
-                MEM <= "001";
+
+            WHEN "000110" => --SW
                 WB <= "0X";
+                MEM <= "001";
+                EX <= "00001";
+
             WHEN "001011" => --Beq
-                EX <= "X0010";
-                MEM <= "1X0";
                 WB <= "XX";
+                MEM <= "1X0";
+                EX <= "X0010";
+
             WHEN "001100" => --Jump
+                WB <= "00";
+                MEM <= "0X0";
+                EX <= "XXXXX";
                 SIGNAL_JUMP <= "01";
-                EX <= "XXXXX";
-                MEM <= "0X0";
-                WB <= "00";
+
             WHEN "001101" => --Jr
+                WB <= "00";
+                MEM <= "0X0";
+                EX <= "XXXXX";
                 SIGNAL_JUMP <= "10";
-                EX <= "XXXXX";
-                MEM <= "0X0";
-                WB <= "00";
+
             WHEN "000000" => -- NOP
-                EX <= "XXXXX";
-                MEM <= "0X0";
                 WB <= "X0";
-            WHEN OTHERS =>
-                EX <= "XXXXX";
                 MEM <= "0X0";
+                EX <= "XXXXX";
+                
+            WHEN OTHERS =>
                 WB <= "00";
+                MEM <= "0X0";
+                EX <= "XXXXX";
+
         END CASE;
     END PROCESS;
 END UC;
